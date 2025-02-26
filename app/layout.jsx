8a8f95e1a3b5afe68c348fd18b1032d755328c59
shahnawaz <shahnawazgull2166@gmail.com>
2025-02-26 23:33:35 +0500

    A

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,3 @@
-import '/styles/landing.css'
-import '/styles/landing_style.css'
-import '/styles/landing-bck.css'
-import '/styles/landing_style-bck.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- components/Landing/Work.jsx
+++ components/Landing/Work.jsx
@@ -3,18 +3,26 @@ import React from 'react';
 const Work = () => {
   return (
     <div>
+        {/* Font Awesome CDN Link */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
       <div className="howitworks" id="HowItWorks">
         {/* Static images */}
-        <img src="/assets/background/Ellipse-2.svg" alt="" className="" />
-        <img src="/assets/background/Ellipse-2.svg" alt="" className="" />
-        <img src="/assets/background/Ellipse-2.svg" alt="" className="" />
-        <img src="/assets/background/Ellipse-2.svg" alt="" className="" />
-        <img src="/assets/background/Ellipse-2.svg" alt="" className="" />
+        <img src="https://vlsmlsaker.s3.amazonaws.com/assets/background/Ellipse%202.svg" alt="" className="" />
+        <img src="https://vlsmlsaker.s3.amazonaws.com/assets/background/Ellipse%202.svg" alt="" className="" />
+        <img src="https://vlsmlsaker.s3.amazonaws.com/assets/background/Ellipse%202.svg" alt="" className="" />
+        <img src="https://vlsmlsaker.s3.amazonaws.com/assets/background/Ellipse%202.svg" alt="" className="" />
+        <img src="https://vlsmlsaker.s3.amazonaws.com/assets/background/Ellipse%202.svg" alt="" className="" />
 
         {/* Heading */}
         <div className="hiw-heading-text">
           How <span className="highlight">VideoCrafter<span style={{ fontSize: '16px' }}>.io</span></span>
-          <span style={{ color: '#000000' }} className="workk">works:</span>
+          <span style={{ color: '#000000' }} className="workk"> Works:</span>
         </div>
 
         {/* Steps Container */}
